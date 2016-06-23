9035f752239369fc83543d92c71feec955f05b03
Oscar Valdivia <catric23@gmail.com>
2016-06-23 16:08:23 +0800

    edited server add app.use

--- server.js
+++ server.js
@@ -2,9 +2,11 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var express = require('express');
 var path = require('path');
 var port = process.env.PORT || 3000;
 
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res){
 	res.sendFile(path.resolve('index.html'));
