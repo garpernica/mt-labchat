a440c682c17439b61044beaec66b82b9d5697dc8
Oscar Valdivia <catric23@gmail.com>
2016-06-25 01:05:19 +0800

    changed json and server

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ var express = require('express');
 var path = require('path');
 var port = process.env.PORT || 3000;
 
+app.set('port',(process.env.PORT || 3000));
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res){
