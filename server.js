579e5aec0f5a56c8ca5c86dd9c89d33f1f3527bd
Oscar Valdivia <catric23@gmail.com>
2016-07-01 18:23:21 +0800

    changed server

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var express = require('express');
 var path = require('path');
 var port = process.env.PORT || 3000;
 
-app.set('port',(process.env.PORT || 3000));
+/*app.set('port',(process.env.PORT || 3000));*/
 
 app.use(express.static(path.join(__dirname, 'public')));
 
